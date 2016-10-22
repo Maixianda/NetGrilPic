b737ad174021b809ede094ae7c7337ddbab910a5
mxd_maidou <359954900@qq.com>
2016-10-22 16:00:55 +0800

    模仿geeknews的retrofit初始化

--- NetGirlPic/app/src/main/java/com/example/maidou/netgirlpic/di/module/AppModule.java
+++ NetGirlPic/app/src/main/java/com/example/maidou/netgirlpic/di/module/AppModule.java
@@ -1,8 +1,14 @@
 package com.example.maidou.netgirlpic.di.module;
 
 import com.example.maidou.netgirlpic.AppContext;
+import com.example.maidou.netgirlpic.di.ContextLife;
+import com.example.maidou.netgirlpic.module.http.RetrofitHelper;
+
+import javax.inject.Singleton;
 
 import dagger.Module;
+import dagger.Provides;
+import retrofit2.Retrofit;
 
 /**
  * Created by Administrator on 2016/10/21.
@@ -12,11 +18,21 @@ import dagger.Module;
  */
 @Module
 public class AppModule {
-    private final AppContext application;
+    private final AppContext appContext;
 
 
     public AppModule(AppContext application) {
-        this.application = application;
+        this.appContext = application;
     }
 
+    @Singleton
+    @Provides
+    @ContextLife("NetGirlPicApplication")
+    AppContext provideApplicationContext() {
+        return appContext;
+    }
+
+    @Singleton
+    @Provides
+    RetrofitHelper provideRetrofitHelper(){return new RetrofitHelper();}
 }
